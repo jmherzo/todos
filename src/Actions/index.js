4e4bb89183195216037d8bf35f91d34158c4fda5
jmherzo <jesus.herzo@outlook.com>
2020-10-18 23:55:52 -0500

    Dispatching Actions Asynchronously with Thunks

--- src/Actions/index.js
+++ src/Actions/index.js
@@ -1,7 +1,7 @@
 import { v4 } from "node-uuid";
 import * as api from '../Api';
 
-export const requestTodos = (filter) => ({
+const requestTodos = (filter) => ({
   type: "REQUEST_TODOS",
   filter
 });
@@ -12,9 +12,12 @@ const receiveTodos = (filter, response) => ({
   response
 });
 
-export const fetchTodos = (filter) => api.fetchTodos(filter).then(response => (
-  receiveTodos(filter, response)
-));
+export const fetchTodos = (filter) => (dispatch) => {
+  dispatch(requestTodos(filter));
+  return api.fetchTodos(filter).then(response => (
+      dispatch(receiveTodos(filter, response))
+  ));
+};
 
 export const toggleTodo = id => ({
   type: "TOOGLE_TODO",
--- src/configureStore.js
+++ src/configureStore.js
@@ -1,14 +1,18 @@
 import todoApp from "./Reducers";
 import { createStore, applyMiddleware } from "redux";
-import promise from "redux-promise";
 import logger from "redux-logger";
 
+const thunk = (store) => (next) => (action) =>
+  typeof action === 'function' ?
+    action(store.dispatch) :
+    next(action);
+
 const configureStore = () => {
-  const middlewares = [promise];
+  const middlewares = [thunk];
   if (process.env.NODE_ENV !== "production") {
     middlewares.push(logger);
   }
- return createStore(todoApp, applyMiddleware(...middlewares));
+  return createStore(todoApp, applyMiddleware(...middlewares));
 };
 
 export default configureStore;
